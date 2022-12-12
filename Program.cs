6fa0f69e66906f594fd4bb78a777776152bfcc25
shubhampachghare <shubhampachghare@gmail.com>
2022-12-12 13:33:20 +0530

    [shubham]- ArrayOfIntDoubleChar

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Generics
+{
+    class program
+    {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------");
+        }
+
+
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+
+            }
+            Console.WriteLine("-----------------------");
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+
+            }
+            Console.WriteLine("-----------------------");
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+            program.toPrint(intArray);
+            program.toPrint(doubleArray);
+            program.toPrint(charArray);
+        }
+    }
+}
