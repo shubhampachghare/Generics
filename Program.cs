f65b6f76ce17968186dbd8f55f0acfd6ca508416
shubhampachghare <shubhampachghare@gmail.com>
2022-12-12 14:31:59 +0530

    [shubham]- Array of int,char ,double using generics

--- Program.cs
+++ Program.cs
@@ -1,47 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Generics
 {
     class program
     {
-        public static void toPrint(int[] inputArray)
+        public static void toPrint<T>(T[] inputArray)
         {
-            foreach (int element in inputArray)
+            foreach (var element in inputArray)
             {
                 Console.WriteLine(element);
             }
-            Console.WriteLine("-----------------------");
-        }
-
-
-
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-
-            }
-            Console.WriteLine("-----------------------");
-        }
-
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
-            {
-                Console.WriteLine(element);
-
-            }
-            Console.WriteLine("-----------------------");
+              Console.WriteLine("----------------------------");
         }
         static void Main(string[] args)
         {
-            int[] intArray = { 1, 2, 3, 4, 5 };
+            int[] intArray = { 1, 2, 3, 4, 5, };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
             char[] charArray = { 'a', 'b', 'c', 'd' };
-            program.toPrint(intArray);
-            program.toPrint(doubleArray);
-            program.toPrint(charArray);
+
+            program.toPrint<int>(intArray);
+            program.toPrint<double>(doubleArray);
+            program.toPrint<char>(charArray);
+
+
         }
-    }
+    }   
 }
